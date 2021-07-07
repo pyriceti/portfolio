b1b6d7b3bb3ac9472ba72c5a7cc4eb0716579aff
Baptiste Perraud <pyriceti@pm.me>
2021-07-07 17:08:16 +0200

    Add phantomjs as dynamic import

--- pages/download-resume.tsx
+++ pages/download-resume.tsx
@@ -3,14 +3,13 @@ import React                            from "react";
 import { componentToPDFBuffer }         from "../util";
 import { ResumeLayout }                 from "../components/resume";
 import pdf                              from "html-pdf";
-import phantomjs                        from "phantomjs";
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res, query }) => {
   const exportPDF = true;
   const isServer = !!req;
 
   const pdfOptions: pdf.CreateOptions = {
-    phantomPath: phantomjs.path,
+    phantomPath: (await import("phantomjs")).path,
     format: "A4",
     orientation: "portrait",
     border: "10mm",
