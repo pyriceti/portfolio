871b2f8f2b1750226a70ba9486967741b94f3f60
Baptiste Perraud <pyriceti@pm.me>
2021-07-13 19:15:18 +0200

    Fix Puppeteer bug when deployed on Vercel

--- pages/api/download-resume.ts
+++ pages/api/download-resume.ts
@@ -1,17 +1,54 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import puppeteer                                from "puppeteer";
 
 async function printPDF(referer: string) {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  await page.goto(referer, { waitUntil: "networkidle0" });
-  const pdf = await page.pdf({ format: "a4" });
-  await browser.close();
-  return pdf;
+  let chrome: any = {};
+  let puppeteer;
+
+  // Running on the Vercel platform
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    chrome = require("chrome-aws-lambda");
+    puppeteer = require("puppeteer-core");
+  }
+  // Running locally
+  else {
+    puppeteer = require("puppeteer");
+  }
+
+  try {
+    let browser;
+
+    // Running on the Vercel platform
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+      browser = await puppeteer.launch({
+        args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+        defaultViewport: chrome.defaultViewport,
+        executablePath: await chrome.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+      });
+    }
+    // Running locally
+    else {
+      browser = await puppeteer.launch({ headless: true });
+    }
+
+    const page = await browser.newPage();
+    await page.goto(referer, { waitUntil: "networkidle0" });
+    const pdf = await page.pdf({ format: "a4" });
+    await browser.close();
+    return pdf;
+
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
 }
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const pdf = await printPDF(req.headers.referer);
+  if (pdf === null)
+    return res.status(500).end();
+
   res.setHeader("Content-Type", "application/pdf");
   res.setHeader("Content-Length", pdf.length);
   res.status(200).send(pdf);
